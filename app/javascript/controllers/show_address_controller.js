99d84f13077e6c797c9febf1ffce42e9dd53d27a
Marvio Rocha <marviorocha@marviorocha.com>
2023-03-12 16:54:14 -0300

    Add loading

--- app/javascript/controllers/show_address_controller.js
+++ app/javascript/controllers/show_address_controller.js
@@ -1,11 +1,33 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="show-address"
 export default class extends Controller {
 
-    handleClick(event) {
-        const element = event.currentTarget
-        const id = element.id
-        console.log(`Clicked element ID: ${id}`)
+    static targets = ['zip', 'address', 'district', 'city', 'state', 'ibge_code']
+
+    connect() {
+
+        this.addressData()
+
+    }
+
+    async addressData(event) {
+
+        const element = event.currentTarget;
+        const id = element.id;
+
+        const response = await fetch(`http://localhost:3000/address/${id}`);
+        const data = await response.json();
+
+        this.zipTarget.innerHTML = data['zip'];
+        this.addressTarget.innerHTML = data['address'];
+        this.districtTarget.innerHTML = data['district'];
+        this.cityTarget.innerHTML = data['city'];
+        this.stateTarget.innerHTML = data['state'];
+        this.ibge_codeTarget.innerHTML = data['ibge_code'];
+
     }
 }
+
+
+
