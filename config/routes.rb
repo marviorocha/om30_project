c91af9579060b903fc8ea168f29ae551619909bd
Marvio Rocha <marviorocha@marviorocha.com>
2023-03-12 16:47:48 -0300

    New: Routes to address

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     root to: 'citizens#index'
 
     resources :citizens, path: 'municipe', only: %i[index new create]
-
+    get 'citizens_cards', to: 'citizens#cards'
+    get 'address/:id', to: 'citizens#address'
 
 end
